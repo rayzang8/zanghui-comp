26f1a157e06ccf0da7b17e4c1d3b8132d8fce056
hui.zang <hui.zang@transwarp.io>
2021-07-31 11:23:49 +0800

    gen d.ts in es

---
+++ es/components/button/index.d.ts
@@ -0,0 +1,3 @@
+import React from 'react';
+import { Button } from 'antd';
+export default function ZanghuiButton(props: React.ComponentProps<typeof Button>): JSX.Element;
---
+++ es/components/card/index.js
@@ -0,0 +1,4 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+export default function ZanghuiCard(props) {
+    return _jsx("div", { children: "Zanghui's card" }, void 0);
+}
