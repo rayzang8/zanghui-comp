71612f8ef826b4f6d1d441edb54c80fda033f84d
hui.zang <hui.zang@transwarp.io>
2021-08-14 14:59:58 +0800

    fix comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import Button from './components/button';
-import './build_bak/main.window';                         // 加载挂载到window上的模块
-import * as varModule from './build_bak/main.var';        // 加载挂默认导出的模块(var 没有暴露出任何模块，相当于自执行函数)
-import * as umdModule from './build_bak/main.umd';        // 加载挂umd模块
-const cmjModule = require('./build_bak/main.commonjs2');  // 加载挂commonjs模块
+import './build_bak/main.window';                         // 挂载到window上的模块
+import * as varModule from './build_bak/main.var';        // 挂载默认导出的模块(var 没有暴露出任何模块，相当于自执行函数)
+import * as umdModule from './build_bak/main.umd';        // 挂载umd模块
+const cmjModule = require('./build_bak/main.commonjs2');  // 挂载commonjs模块
 const promiseAmdModule = import('./build_bak/main.amd');  // 异步加载amd模块, 可能需要依赖systtemjs，@types/systemjs 库
 
 function App() {
